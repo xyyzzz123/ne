67e5a306f1d5293be6a6d66ec54493634ac4ad4b
xyyzzz123 <xmyiwz@163.com>
2018-04-17 12:04:44 +0800

    add time variable

--- dy.py
+++ dy.py
@@ -3,10 +3,16 @@ import matplotlib.pyplot as plt
 
 
 a = 5
-pm = 9 / 10
-b = 6
-pn = 9 / 10
+# pm = 95 / 100 # 9 / 10
+# pm = 9 / 10
+pm = [95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100]
+# pm = [9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10]
 
+b = 6
+# pn = 95 / 100 # 9 / 10
+# pn = 9 / 10
+pn = [95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100, 95 / 100]
+# pn = [9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10, 9 / 10]
 
 def one_sided():
     a = 5
@@ -27,12 +33,13 @@ def one_sided():
     plt.show()
 
 
-def f(x):
-    return 1 - pm / (1 + a * x ** 2)
+
+def f(x, t):
+    return 1 - pm[t] / (1 + a * x ** 2)
 
 
-def g(x):
-    return 1 - pn / (1 + b * x ** 2)
+def g(x, t):
+    return 1 - pn[t] / (1 + b * x ** 2)
 
 
 def two_sided():
@@ -56,30 +63,37 @@ def two_sided():
 def simulate():
     x = 0
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
+    # fig = plt.figure()
+    fig, (ax, bx) = plt.subplots(2, figsize=(5, 8))
+
+    # ax = fig.add_subplot(1, 1, 1)
+    # bx = fig.add_subplot(1, 1, 1)
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
+    bx.set_ylim(0, 1)
+
     plt.title('test')
     plt.grid(True)
 
     n = np.arange(0, 1, 0.005)
-    m = 1 - pm / (1 + a * n ** 2)
+    m = 1 - pm[0] / (1 + a * n ** 2)
     ax.plot(n, m)
 
     m = np.arange(0, 1, 0.005)
-    n = 1 - pn / (1 + b * m ** 2)
+    n = 1 - pn[0] / (1 + b * m ** 2)
     ax.plot(n, m)
 
     i = 0
-    while(x != g(f(x))):
-        ax.scatter(x, f(x))
-        ax.annotate('(%.2f, %.2f)' % (x, f(x)), xy = (x, f(x)), textcoords='data')
-        x = f(x)
+    while(abs(x - g(f(x, i), i)) > 0.001):
+        ax.scatter(x, f(x, i))
+        bx.scatter(i, f(x, i), c='r')
+        ax.annotate('(%.2f, %.2f)' % (x, f(x, i)), xy = (x, f(x, i)), textcoords='data')
+        x = f(x, i)
 
-        ax.scatter(g(x), x)
-        ax.annotate('(%.2f, %.2f)' % (g(x), x), xy=(g(x), x), textcoords='data')
-        x = g(x)
+        ax.scatter(g(x, i), x)
+        bx.scatter(i, g(x, i), c='b')
+        ax.annotate('(%.2f, %.2f)' % (g(x, i), x), xy=(g(x, i), x), textcoords='data')
+        x = g(x, i)
         print(x)
         i += 1
 
@@ -92,3 +106,4 @@ def simulate():
 if __name__ == '__main__':
     simulate()
     # two_sided()
+    # one_sided()
