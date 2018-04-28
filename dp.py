e3c643f38b938fabdbd86a608bc73afff2a324e4
xyyzzz123 <xmyiwz@163.com>
2018-04-28 21:01:19 +0800

    fix bugs

--- dp.py
+++ dp.py
@@ -1,10 +1,6 @@
-import numpy as np
-
-a = 5
-b = 5
 
 
-def dp(steps):
+def dp(steps, a, b):
     dp, plist, index = {}, {}, {}
     dp[0] = {0: 0} # m[0] = 0
 
@@ -24,7 +20,7 @@ def dp(steps):
 
     max_profit, key = 0, 0
     for (k, v) in dp[steps].items():
-        if v > max_profit:
+        if v >= max_profit:
             max_profit = v
             key = k
 
@@ -46,68 +42,26 @@ def dp(steps):
     print(profits)
 
 
-# def dp_mn(steps):
-#     dp, plist, index = {}, {}, {}
-#     dp[0] = {(0, 0): 0}  # dp[step][(m, n)] = max profit at (m, n)
-#
-#     for i in range(1, steps + 1):
-#         dp[i], plist[i], index[i] = {}, {}, {}
-#
-#         for j in range(0, 10):
-#             pm = j / 10
-#             for k in range(0, 10):
-#                 pn = k / 10
-#
-#                 for ((m, n), v) in dp[i - 1].items():
-#                     mm = round(1 - pm / (1 + a * n ** 2), 1)
-#                     nn = round(1 - pn / (1 + b * m ** 2), 1)
-#
-#                     profit = v + (mm - m) * pm + (nn - n) * pn
-#                     if profit >= dp[i].get((mm, nn), 0):
-#                         dp[i][(mm, nn)] = profit
-#                         plist[i][(mm, nn)] = (pm, pn)
-#                         index[i][(mm, nn)] = (m, n)
-#
-#                     dp[i][(mm, nn)] = round(dp[i][(mm, nn)], 4)
-#                     print("(%.2f, %.2f): %.4f" % (mm, nn, dp[i][(mm, nn)]))
-#
-#     max_profit = 0
-#     key = (0, 0)
-#     for (k, v) in dp[steps].items():
-#         if v > max_profit:
-#             key = k
-#             max_profit = v
-#
-#     i = steps
-#     prices = []
-#     while i > 0:
-#         prices.append(plist[i][key])
-#         key = index[i][key]
-#         i -= 1
-#     prices.reverse()
-#
-#     print(max_profit)
-#     print(prices)
-
-
-def dp_mn(steps):
+def dp_mn(steps, a, b):
     dp, plist, index = {}, {}, {}
     dp[0] = {(0, 0): 0}  # dp[step][(m, n)] = max profit at (m, n)
+    fw = open("./output.txt", "a")
 
     for i in range(1, steps + 1):
         dp[i], plist[i], index[i] = {}, {}, {}
         for ((m, n), v) in dp[i - 1].items():
 
             for j in range(0, 100):
-                pm = j / 100
                 for k in range(0, 100):
+                    pm = j / 100
                     pn = k / 100
                     print("%d (pm, pn) = (%.2f, %.2f)" % (i, pm, pn))
+
                     mm = round(1 - pm / (1 + a * n ** 2), 2)
                     nn = round(1 - pn / (1 + b * m ** 2), 2)
 
                     profit = v + (mm - m) * pm + (nn - n) * pn
-                    if profit >= dp[i - 1].get((mm, nn), 0):
+                    if profit >= dp[i].get((mm, nn), 0):
                         dp[i][(mm, nn)] = round(profit, 2)
                         plist[i][(mm, nn)] = (pm, pn)
                         index[i][(mm, nn)] = (m, n)
@@ -118,7 +72,7 @@ def dp_mn(steps):
     max_profit = 0
     key = (0, 0)
     for (k, v) in dp[steps].items():
-        if v > max_profit:
+        if v >= max_profit:
             key = k
             max_profit = v
 
@@ -126,7 +80,7 @@ def dp_mn(steps):
     prices, population, profits = [], [], []
     while i > 0:
         prices.append(plist[i][key])
-        population.append(index[i][key])
+        population.append(key)
         profits.append(dp[i][key])
         key = index[i][key]
         i -= 1
@@ -139,15 +93,22 @@ def dp_mn(steps):
     print("population: " + str(population))
     print("profits: " + str(profits))
 
+    fw.write("a = %d, b = %d\n" % (a, b))
+    fw.write(str(dp[steps]) + "\n")
+    fw.write(str(max_profit) + "\n")
+    fw.write("prices: " + str(prices) + "\n")
+    fw.write("population: " + str(population) + "\n")
+    fw.write("profits: " + str(profits) + "\n\n")
+    fw.close()
+
+
 
 if __name__ == '__main__':
-    # dp(13)
-    # dp(13)
-    # l = []
-    # l.append({})
-    # l[0] = {1:2}
-    dp_mn(13)
-    # tt = {}
-    # tt[(0, 0)] = [2,3]
-    # tt[(0, 1)] = tt[(0, 0)].append(1)
-    # print(tt)
+    steps = 10
+    for b in range(1, 11):
+        dp_mn(steps, 5, b)
+
+    for a in range(1, 11):
+        if a == 5: continue
+        for b in range(1, 11):
+            dp_mn(steps, a, b)
